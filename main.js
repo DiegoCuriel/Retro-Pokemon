46dd594a3d480caf0926652d297f21aa6a09b003
Curiel <a01640372@tec.mx>
2023-02-27 21:47:41 -0600

    JQuery info slides

--- main.js
+++ main.js
@@ -1,6 +1,49 @@
 $(function()
 {
+    $("#logo").mouseenter(function()
+    {
+        $("#logo-info").slideDown();
+    });
+    $("#logo").mouseleave(function()
+    {
+        $("#logo-info").slideUp();
+    });
+    
+    $("#raichu").mouseenter(function()
+    {
+        $("#raichu-info").slideDown();
+    });
+    $("#raichu").mouseleave(function()
+    {
+        $("#raichu-info").slideUp();
+    });
 
+    $("#gengar").mouseenter(function()
+    {
+        $("#gengar-info").slideDown();
+    });
+    $("#gengar").mouseleave(function()
+    {
+        $("#gengar-info").slideUp();
+    });
+
+    $("#giratina").mouseenter(function()
+    {
+        $("#giratina-info").slideDown();
+    });
+    $("#giratina").mouseleave(function()
+    {
+        $("#giratina-info").slideUp();
+    });
+
+    $("#magicarp").mouseenter(function()
+    {
+        $("#magicarp-info").slideDown();
+    });
+    $("#magicarp").mouseleave(function()
+    {
+        $("#magicarp-info").slideUp();
+    });
 });
 
 function playAudio()
